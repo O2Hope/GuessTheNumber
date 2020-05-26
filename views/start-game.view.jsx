7dc072ff51ac446999563e028d218ae463ad5d5b
Agustin Ruiz <Agustin.Ruiz@ey.com>
2020-05-25 22:25:28 -0500

    Create app header and main view

---
+++ views/start-game.view.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const StartGame = () => (
+  <View style={styles.screen}>
+    <Text>Game Screen</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+});
+
+export default StartGame;
